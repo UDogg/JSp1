d3ff3280f127944032f058e5e2242530379230ca
Utkarsh Choudhary <ufc5009@psu.edu>
2023-02-27 00:06:58 -0500

    Create Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,30 @@
+import "./Form.css";
+import React from 'react'
+import { render } from "@testing-library/react";
+
+const Form = () => {
+    render()
+    {
+        return (
+            <div className="Form">
+                <h1>This is a mock contact page like Big-Tech companies.</h1>
+                <form>
+                    <label>Your Name</label>
+                    <input type="text"></input>
+                    <label>E-Mail</label>
+                    <input type="email"></input>
+                    <label>Subject</label>
+                    <input type="text"></input>
+                    <label>Message</label>
+                    <textarea rows="6" placeholder ="Type Your Message Here"/>
+                    <button className="btn">Submit</button>
+                </form>
+        
+            </div>
+          )
+
+    }
+
+}
+
+export default Form
