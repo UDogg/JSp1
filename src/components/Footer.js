2ee5157070f927b16fe91ee782a9ea57494a73ec
Utkarsh Choudhary <ufc5009@psu.edu>
2023-04-25 17:15:05 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -49,7 +49,8 @@ const Footer = () => {
         <div className="right">
           <h4>About Me</h4>
           <p>
-            Hi, My Name is Utkarsh(UC) Choudhary and this is my resumé https://ipfs.io/ipfs/QmbwGMYrckUzTwcP8bTxRMrLqxqaUATPs7xUqsAWacFEUM?filename=CV_Utkarsh%20Choudhary.pdf
+            Hi, My Name is Utkarsh(UC) Choudhary and this is my 
+            <a href="https://ipfs.io/ipfs/QmbwGMYrckUzTwcP8bTxRMrLqxqaUATPs7xUqsAWacFEUM?filename=CV_Utkarsh%20Choudhary.pdf">Resumé!</a>
           </p>
         </div>
       </div>
