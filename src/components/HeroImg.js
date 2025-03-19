acfea01247d1f43b491a6cc40390c4a8066f7266
Utkarsh Choudhary <ufc5009@psu.edu>
2025-03-19 03:26:54 -0400

    Update HeroImg.js

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -13,12 +13,17 @@ const HeroImg = () => {
                 </div>
                 <div className="content">
                     <p>
-                        Hi, My name is UC Choudhary.
+                        Hi, My name is Utkarsh Choudhary.
                     </p>
                     <h1>
-                        I am a Computer Science Student at Penn State.
+                        I was a Computer Science Student at Penn State.
 
                     </h1>
+                    <h1> 
+            This site was just for a school project and is not maintained. For the currently maintained site, please visit 
+            <a href="https://ucportfolioiitb.netlify.app/">my new site: "https://ucportfolioiitb.netlify.app/"
+            </a>
+            </h1>
                     <div className="btncont">
                         <Link to ="/Projects" className="btn">Projects</Link>
                         <Link to ="/Contact" className="btn btn-light">Contact</Link>
